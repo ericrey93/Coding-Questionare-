6437d70bf79ae5de9fcde7e78c005b32f468719f
Eric Reyes <ericreyes1109@gmail.com>
2022-12-22 12:52:30 -0600

    adding all the updated files

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,15 @@ var content = document.querySelector('.content');
 var questionText = document.querySelector('.questionText');
 var startBtn = document.getElementById("start-Btn");
 var nextBtn = document.getElementById('next-btn');
-var choiceA = document.getElementsByClassName('choiceA')
-var choiceB = document.getElementsByClassName('choiceB');
-var choiceC = document.getElementsByClassName('choiceC');
-var choiceD = document.getElementsByClassName('choiceD');
+var choiceA = document.querySelector('.choiceA');
+var choiceB = document.querySelector('.choiceB');
+var choiceC = document.querySelector('.choiceC');
+var choiceD = document.querySelector('.choiceD');
+var checkedA = document.querySelector('#A');
+var checkedB = document.querySelector('#B');
+var checkedC = document.querySelector('#C');
+var checkedD = document.querySelector('#D');
+var highscore = 0
 var firstQuestion = 
     {
         question:"When creating an array in Javascript, what will the first index have a value of?",
@@ -46,18 +51,68 @@ function startGame() {
     startBtn.classList.add('hide');
     content.classList.add('hide');
     nextBtn.classList.add('show');
+    var timerStart = 45;
+    var startTimer = setInterval(function() {
+        timerStart--;
+        timer.textContent = timerStart;
+        if(timerStart <= 0)
+        clearInterval(startTimer);
+    }, 1000);
     questionText.textContent = firstQuestion.question;
     choiceA.textContent = firstQuestion.choice1;
+    choiceB.textContent = firstQuestion.choice2;
+    choiceC.textContent = firstQuestion.choice3;
+    choiceD.textContent = firstQuestion.choice4;
 }
 
-function nextQuestion() {
+function secondQuestion() {
+    if ((checkedA.checked) ||
+    (checkedB.checked) ||
+    (checkedD.checked)) {
+        window.alert("wrong!")
+        checkedA.checked = false;
+        checkedB.checked = false;
+        checkedD.checked = false;
+questionText.textContent = nextQuestions[0].question;
+choiceA.textContent = nextQuestions[0].choice1;
+choiceB.textContent = nextQuestions[0].choice2;
+choiceC.textContent = nextQuestions[0].choice3;
+choiceD.textContent = nextQuestions[0].choice4;
+
+} else if(checkedC.checked) {
+    window.alert("correct!")
+    checkedC.checked = false;
+    highscore++;
+questionText.textContent = nextQuestions[0].question;
+choiceA.textContent = nextQuestions[0].choice1;
+choiceB.textContent = nextQuestions[0].choice2;
+choiceC.textContent = nextQuestions[0].choice3;
+choiceD.textContent = nextQuestions[0].choice4;
 }
 
-
-function selectAnswer() {
-
-}
+if((checkedB.checked) ||
+(checkedC.checked) ||
+(checkedD.checked)) {
+    window.alert("wrong!")
+    checkedB.checked = false;
+    checkedC.checked = false;
+    checkedD.checked = false;
+    questionText.textContent = nextQuestions[1].question;
+choiceA.textContent = nextQuestions[1].choice1;
+choiceB.textContent = nextQuestions[1].choice2;
+choiceC.textContent = nextQuestions[1].choice3;
+choiceD.textContent = nextQuestions[1].choice4;
+} else if(checkedA.checked) {
+    window.alert("correct!")
+    checkedA.checked = false;
+    highscore++;
+questionText.textContent = nextQuestions[1].question;
+choiceA.textContent = nextQuestions[1].choice1;
+choiceB.textContent = nextQuestions[1].choice2;
+choiceC.textContent = nextQuestions[1].choice3;
+choiceD.textContent = nextQuestions[1].choice4;
+}}
 
 
 startBtn.addEventListener('click', startGame);
-nextBtn.addEventListener('click', nextQuestion);
+nextBtn.addEventListener('click', secondQuestion);
